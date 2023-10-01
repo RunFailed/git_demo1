0876901b63739291411f378ed568b58ac5dc91af
zz <wuzz0179@gmail.com>
2023-10-01 23:32:02 +0800

    second_hot_fix

--- hello.cpp
+++ hello.cpp
@@ -14,6 +14,7 @@ int main()
         cout << "Hello,world!" << endl;
         cout << "222222222222" << endl;
         cout << "333333333333" << endl;
-        cout << "first fix" << endl;
+        cout << "first fix" << endl;    
+        // second hot fix
         return 0;
 }
