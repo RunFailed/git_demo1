23d60c131da3b4ad52886451786708c595c297cd
zz <wuzz0179@gmail.com>
2023-10-01 22:21:42 +0800

    second commit

--- hello.cpp
+++ hello.cpp
@@ -12,5 +12,6 @@ int main()
         cout << "Hello,world!" << endl;
         cout << "Hello,world!" << endl;
         cout << "Hello,world!" << endl;
+        cout << "222222222222" << endl;
         return 0;
 }
