2087326cbbbcf5c08ef07157840a5530fe279d7b
zz <wuzz0179@gmail.com>
2023-10-01 22:06:24 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+        cout << "Hello,world!" << endl;
+        cout << "Hello,world!" << endl;
+        cout << "Hello,world!" << endl;
+        cout << "Hello,world!" << endl;
+        cout << "Hello,world!" << endl;
+        cout << "Hello,world!" << endl;
+        cout << "Hello,world!" << endl;
+        cout << "Hello,world!" << endl;
+        return 0;
+}
